ecf0394183c9e94c744044463cd4d8416bae9142
joheee <johevinblesstowi07@gmail.com>
2025-11-10 06:22:26 +0700

    initialize swagger

--- main.go
+++ main.go
@@ -4,15 +4,36 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	docs "github.com/joheee/portfolio-backend/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /ping [get]
+func getPing(g *gin.Context){
+	g.JSON(http.StatusOK, gin.H{
+		"message":"pong",
+	})
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":"pong",
-		})
-	})
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/ping", getPing)
+	}
 
-	r.Run()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	r.Run(":8080")
 }
